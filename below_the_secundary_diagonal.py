7e925a77c8a36acdd737f1ef38be1f06a1438c78
Marcelo Saad <marcelo.saad@sasi.com.br>
2023-02-01 11:47:13 -0400

    1186 beecrowd

---
+++ below_the_secundary_diagonal.py
@@ -0,0 +1,49 @@
+'''
+Read an uppercase character that indicates an operation that will be performed in an array M[12][12]. Then, calculate and print the sum or average considering only that numbers that are below of the Secundary diagonal of the array, like shown in the following figure (green area).
+
+
+
+
+Input
+
+The first line of the input contains a single uppercase character O ('S' or 'M'), indicating the operation Sum or Average (Média in portuguese) to be performed with the elements of the array. Follow 144 floating-point numbers of the array.
+
+Output
+
+Print the calculated result (sum or average), with one digit after the decimal point.
+
+Input Sample	Output Sample
+S
+5.0
+0.0
+-3.5
+2.5
+4.1
+...
+                12.6
+'''
+
+m=[]
+letra = input()
+
+for i in range(12):
+    m.append([])
+    for j in range(12):
+        x = float(input())
+        m[i].append(x)
+c = 0
+s = 0
+cont = 0
+for i in range(11,0,-1):
+    c +=1
+    for j in range(c,12):
+        s += m[i][j]
+        cont += 1
+
+if letra == 'S':
+    print('{}'.format(s))
+
+if letra == 'M':
+    med = s / cont
+    print('{:.1f}'.format(med))
+    
