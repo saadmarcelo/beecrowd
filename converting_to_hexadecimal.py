39aab8c1bce5dacec78dbc8a038ffba94e580516
Marcelo Saad <marcelo.saad@sasi.com.br>
2023-02-16 11:30:00 -0400

    beecrowd python 1957

---
+++ converting_to_hexadecimal.py
@@ -0,0 +1,27 @@
+'''
+Data stored in computers are in binary. An economic way of visualizing these numbers is the usage of base 16 (hexadecimal).
+
+Your task is to write a program that, given a natural number at base 10, shows its representation in hexadecimal.
+
+Input
+
+The input is a positive integer number V at base 10 (1 ≤ V ≤ 2 x 109).
+
+Output
+
+The output is the same number V at base 16 in a single line (don't forget the end-of-line character). Use uppercase letters, as shown in the examples.
+
+Input Samples	Output Samples
+10
+                A
+15
+                F
+16
+                10
+31
+                1F
+65535
+                FFFF
+'''
+v= int(input())
+print(hex(v).lstrip("0x").rstrip("L").upper())
