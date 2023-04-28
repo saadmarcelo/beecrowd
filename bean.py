3d0417f533042ca209df87fad19da3726e507dad
Marcelo Saad <marcelo.saad@sasi.com.br>
2023-04-28 12:30:23 -0400

    beecrowd 2791 python

---
+++ bean.py
@@ -0,0 +1,34 @@
+'''
+It is said in the surroundings of Montes Claros that, long ago in the municipal market, Sebastião and his companions of work always play a game of divination after the delivery of agricultural products harvested in the week that happened. The game, "Guess Where the Bean is", consists in hiding a grain of beans in one of four opaque glasses, and after shuffling them, the bettor must guess in which glass the vegetable is.
+
+
+
+This year, due to the great cultural and historical success and the enormous amount of people who practice this game in the municipal market, the city council decided to hold a "Guess Where the Bean is '' championship. However, she needs a program to show viewers where the beans were after the end of a game. Knowing that the next Programming Marathon will take place in the city, she soon commissioned a solution from the excellent programmers. In this way, you should assist the organization in this mission with a program that will inform, at the end of a match, where the beans were.
+
+Input
+
+The entry will contain only one line with four integers, C1, C2, C3 and C4 separated by a space. The value Ci = 1 indicates that the beans were in cup number i, and Ci = 0 indicates that the ith cup was empty at the end of the game. There will always be exactly one glass with the beans.
+
+Output
+
+Write in the output a line containing an integer between 1 and 4, corresponding to the position where the beans were.
+
+Input Samples	Output Samples
+0 0 0 1
+                4
+0 1 0 0
+                2
+0 0 1 0
+                3
+'''
+
+C1,C2,C3,C4 = map(int,input().split())
+
+if C1 ==1:
+    print("1")
+if C2 ==1:
+    print("2")
+if C3 ==1:
+    print("3")
+if C4 ==1:
+    print("4")
